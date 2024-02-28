e90e796b98599ffeb7af8ee4fa16f5199b98e126
pantheepatel <patel.panthee.18@gmail.com>
2024-02-29 00:06:27 +0530

    added .env file

--- plant-hub-frontend/src/Config.js
+++ plant-hub-frontend/src/Config.js
@@ -10,21 +10,21 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDIV4i7XUCCXeQA92NI12hpZwpqmdXZKeI",
-  authDomain: "planthub-95346.firebaseapp.com",
-  projectId: "planthub-95346",
-  storageBucket: "planthub-95346.appspot.com",
-  messagingSenderId: "113732661573",
-  appId: "1:113732661573:web:2a5df5910f87016a700e2c",
-  measurementId: "G-CTGBT834CJ"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app)
-const isDevelopment = (process.env.NODE_ENV === 'development' ? true : false)
-console.log('is development : ',isDevelopment)
+// const isDevelopment = (process.env.NODE_ENV === 'development' ? true : false)
+// console.log('is development : ',isDevelopment)
 
 // const db = getFirestore(app);
 
